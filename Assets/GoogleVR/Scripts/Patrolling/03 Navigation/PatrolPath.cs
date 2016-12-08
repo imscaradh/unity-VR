797786a3305b20f923ecb9ba7efaf3208b8268d2
Pascal Zingg <pazingg@gmail.com>
2016-12-08 22:51:17 +0100

    Initial commit

---
+++ Assets/GoogleVR/Scripts/Patrolling/03 Navigation/PatrolPath.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+
+using System.Collections.Generic;
+
+public class PatrolPath : MonoBehaviour
+{
+    public enum Direction
+    {
+        Left,
+        Right
+    }
+
+    // Initial direction of this path
+    public Direction direction = Direction.Right;
+
+    public List<Transform> waypoints;
+
+    // weather or not to connect start and end point togeter
+    public bool closedPath = false;
+
+    // current direction indicator
+    private bool _goingRight;
+
+    private int _currentIndex = 0;
+
+    void Awake()
+    {
+        _goingRight = (direction == Direction.Right);
+    }
+
+    // property to get the current waypoint if it exists
+    public Transform currentWaypoint
+    {
+        get
+        {
+            if (waypoints.Count > 0)
+                return waypoints[_currentIndex];
+
+            return null;
+        }
+    }
+
+    // proerty to get the last waypoint if it exists
+    public Transform lastWaypoint
+    {
+        get
+        {
+            if (waypoints.Count > 0)
+                return waypoints[waypoints.Count - 1];
+
+            return null;
+        }
+    }
+
+    // adds a new waypoint
+    public void AddWaypoint()
+    {
+        // create a new gameobject and call it waypoint 
+        // note: this will not generate unique names.
+        GameObject go = new GameObject("Waypoint " + waypoints.Count);
+        go.transform.parent = transform;
+        go.transform.localPosition = Vector3.zero;
+
+        if (waypoints.Count > 0)
+        {
+            Vector3 direction = Vector3.forward;
+            // get direction of last edge
+            if (waypoints.Count > 1)
+            {
+                Vector3 from = waypoints[waypoints.Count - 2].position;
+                Vector3 to = lastWaypoint.position;
+                direction = (to - from).normalized;
+            }
+
+            go.transform.position = lastWaypoint.position + direction * 0.5f;
+        }
+
+        // Add a waypoint component to the gameobject and add it's reference to our list
+        waypoints.Add(go.transform);
+    }
+
+    public void Next()
+    {
+        IncrementIndex();
+    }
+
+    // increment the index basd on closePath etc.
+    void IncrementIndex()
+    {
+        if (!closedPath &&
+                (_goingRight && _currentIndex == waypoints.Count - 1
+                || !_goingRight && _currentIndex == 0))
+        {
+            _goingRight = !_goingRight;
+        }
+
+        if (_goingRight) _currentIndex++;
+        else _currentIndex--;
+
+        while (_currentIndex < 0)
+            _currentIndex += waypoints.Count;
+
+        _currentIndex %= waypoints.Count;
+    }
+		
+   
+}
---
+++ Assets/GoogleVR/Scripts/Patrolling/AgentAIBehaviour.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentAIBehaviour : MonoBehaviour
+{
+	public PatrolPath path;
+	public float patrolSpeed = 3.5f;
+	public float reachedDistance = 1.2f;
+	private NavMeshAgent _agent;
+
+	public enum State
+	{
+		Patrolling,
+		Chasing,
+		Searching
+	}
+
+	public AgentSight sight;
+	public float chaseSpeed = 6.0f;
+	public float alertLevelDecreaseTime = 10.0f;
+	private State _state = State.Patrolling;
+	private float _alertLevel = 0.0f;
+	private Transform _player;
+	private Vector3 _lastPositionOfInterest;
+
+	void  Awake ()
+	{
+		_agent = GetComponent< NavMeshAgent > ();
+		_player = GameObject.FindGameObjectWithTag ("MainCamera").transform;
+	}
+
+	void  Update ()
+	{
+		Patrol ();
+		switch (_state) {
+		case  State .Patrolling:
+			Patrol ();
+			break;
+		case  State .Chasing:
+			Chase ();
+			break;
+		case  State .Searching:
+			Search ();
+			break;
+		}
+	}
+
+	void Patrol ()
+	{
+		// Reset the look direction to the z-axis
+		sight.LookForward ();
+
+		_agent.speed = patrolSpeed;
+		Vector3 dest = path.currentWaypoint.transform.position;
+		Debug.Log (dest);
+		_agent.SetDestination (dest);
+		// Set the next destination point if we are closer than a threshold
+		if (Vector3.Distance (dest, transform.position) < reachedDistance)
+			path.Next ();
+	}
+
+	void OnEnable ()
+	{
+		sight.playerSpotted += PlayerSpotted;
+	}
+
+	void OnDisable ()
+	{
+		sight.playerSpotted -= PlayerSpotted;
+	}
+
+	void  PlayerSpotted (Vector3 position)
+	{
+		_state = State.Chasing;
+		_lastPositionOfInterest = position;
+	}
+
+	void Chase ()
+	{
+		// if player is not in sight and we arrived at the last position he was seen at
+		// then switch to searching the area
+		if (!sight.playerInSight && _agent.remainingDistance <= _agent.stoppingDistance) {
+			_state = State.Searching;
+			return;
+		}
+		// always look at the current player position while chasing
+		sight.LookAtPosition (_player.position);
+		// update speed
+		_agent.speed = chaseSpeed;
+		// update agent destination
+		_agent.SetDestination (_lastPositionOfInterest);
+		// keep alert level on maximum
+		_alertLevel = 1.0f;
+	}
+
+	void Search ()
+	{
+		// set speed to a lerp between chase and patrol speed based on alert level
+		_agent.speed = patrolSpeed + _alertLevel * (chaseSpeed - patrolSpeed);
+		// decrease alert level over time
+		if (alertLevelDecreaseTime > 0.0f)
+			_alertLevel -= Time.deltaTime / alertLevelDecreaseTime;
+		else
+			_alertLevel = 0.0f;
+		if (_alertLevel <= 0.0f) {
+			_alertLevel = 0.0f;
+			_state = State.Patrolling;
+		}
+		// keep looking at the current target
+		sight.LookAtPosition (_lastPositionOfInterest);
+		_agent.SetDestination (_lastPositionOfInterest);
+	}
+}
