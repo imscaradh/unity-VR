c8e48207c1f827e21b831e2417b04a96ba0bd47e
Pascal Zingg <pazingg@gmail.com>
2016-12-15 15:07:52 +0100

    Updates movement controller (distinguish between normal and speedup mode) and inserts GUI (no functionallity yet)

--- Assets/GoogleVR/Scripts/MovementController.cs
+++ Assets/GoogleVR/Scripts/MovementController.cs
@@ -8,25 +8,45 @@ public class MovementController : MonoBehaviour
 	public float speed;
 	public float speedUp;
 
-	private GvrHead head;
+	private bool speedUpActive = false;
+
+	GameObject menuCanvas;
 
 	void Start ()
 	{
-		head = Camera.main.GetComponent<StereoController> ().Head;
+		menuCanvas = GameObject.FindWithTag ("Menu");
 	}
 
 	void Update ()
 	{
-		if (GvrViewer.Instance.Triggered) {
-			Walk (speedUp);
-		} else {
-			Walk (speed);
+		MoveMenu ();
+
+		// TODO: Improve if-else (could be smarter)
+		if (GvrViewer.Instance.Triggered && speedUpActive) {
+			speedUpActive = false;
+			Debug.Log ("Speedup deactivated");
+		} else if (GvrViewer.Instance.Triggered && !speedUpActive) {
+			speedUpActive = true;
+			Debug.Log ("Speedup activated");
 		}
+		Walk (speedUpActive ? speedUp : speed);
+	}
+
+	void MoveMenu ()
+	{
+		// Position
+		Vector3 pos = transform.position;
+		pos.y = menuCanvas.transform.position.y;
+		menuCanvas.transform.position = pos;
+
+		// Rotation
+		Vector3 rotAngles = menuCanvas.transform.eulerAngles;
+		rotAngles.y = transform.eulerAngles.y;
+		menuCanvas.transform.eulerAngles = rotAngles;
 	}
 
 	void Walk (float _speed)
 	{
-		Debug.Log (_speed);
 		transform.position += transform.forward * _speed * Time.deltaTime;
 	}
 }
