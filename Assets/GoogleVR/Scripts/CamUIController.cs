0d9239dd48b0eaff040c117d22dcee093b4f2bb2
Pascal Zingg <pazingg@gmail.com>
2016-12-28 17:08:32 +0100

    Fixes camera breakthrough (decreases z-Distance)

--- Assets/GoogleVR/Scripts/CamUIController.cs
+++ Assets/GoogleVR/Scripts/CamUIController.cs
@@ -7,21 +7,17 @@ public class CamUIController : MonoBehaviour
 {
 	public RawImage rawimage;
 
-	private Camera mainCamera;
-
 	void Start ()
 	{
 		WebCamTexture webcamTexture = new WebCamTexture ();
 		rawimage.texture = webcamTexture;
 		rawimage.material.mainTexture = webcamTexture;
 		webcamTexture.Play ();
-
-		mainCamera = GameObject.FindWithTag ("MainCamera").GetComponent<Camera> ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		rawimage.rectTransform.SetAsLastSibling ();
+		
 	}
 }
