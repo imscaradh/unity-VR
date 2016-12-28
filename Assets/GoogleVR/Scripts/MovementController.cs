7a586103efe77a06b0f488d97f83784149ed6413
Pascal Zingg <pazingg@gmail.com>
2016-12-28 16:31:22 +0100

    Adds Camera view

--- Assets/GoogleVR/Scripts/MovementController.cs
+++ Assets/GoogleVR/Scripts/MovementController.cs
@@ -10,20 +10,13 @@ public class MovementController : MonoBehaviour
 
 	private bool speedUpActive = false;
 
-	GameObject menuCanvas;
 
 	void Start ()
 	{
-		WebCamDevice[] devices = WebCamTexture.devices;
-		for( int i = 0 ; i < devices.Length ; i++ )
-			Debug.Log(devices[i].name);  
-		
-		menuCanvas = GameObject.FindWithTag ("Menu");
 	}
 
 	void Update ()
 	{
-		MoveMenu ();
 
 		// TODO: Improve if-else (could be smarter)
 		if (GvrViewer.Instance.Triggered && speedUpActive) {
@@ -36,18 +29,6 @@ public class MovementController : MonoBehaviour
 		Walk (speedUpActive ? speedUp : speed);
 	}
 
-	void MoveMenu ()
-	{
-		// Position
-		Vector3 pos = transform.position;
-		pos.y = menuCanvas.transform.position.y;
-		menuCanvas.transform.position = pos;
-
-		// Rotation
-		Vector3 rotAngles = menuCanvas.transform.eulerAngles;
-		rotAngles.y = transform.eulerAngles.y;
-		menuCanvas.transform.eulerAngles = rotAngles;
-	}
 
 	void Walk (float _speed)
 	{
